1dd7643ab241e4b97440e696cf92cd97c9baff31
Denis Dumont <47156341+denisdums@users.noreply.github.com>
2023-06-19 09:33:34 +0200

    Feature/improve api performances (#7)
    * improve api performance with priceline API
    
    * cities update
    
    ---------
    
    Co-authored-by: Denis Dumont <denis.dumont09@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './app/search/search.module';
 import { ConfigModule } from '@nestjs/config';
+import { CitiesModule } from './app/cities/cities.module';
 
 ConfigModule.forRoot({
   isGlobal: true,
 });
 
 @Module({
-  imports: [SearchModule],
+  imports: [SearchModule, CitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
